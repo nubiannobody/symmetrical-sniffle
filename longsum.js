5428214670f2e056fb665c91f8460a1b1e31bcdc
Kween <kweenyoallhiada@gmail.com>
2023-02-05 23:38:28 -0800

    Add comments

--- longsum.js
+++ longsum.js
@@ -1,4 +1,4 @@
-
+// Write a function that shows us two numbers being added. Use template literals to build a string that includes the two numbers and the final sum. (Because you're building up strings, pay attention to the spacing!)
 function longSum(x,y) {
     let sum = x + y;
     return `${x} + ${y} = ${sum}`
