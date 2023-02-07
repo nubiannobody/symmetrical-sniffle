cdb2e7e137efceea8ff7002bea4857c8c397cc89
Kween <kweenyoallhiada@gmail.com>
2023-02-07 07:19:30 -0800

    Add betty.js

---
+++ betty.js
@@ -0,0 +1,8 @@
+const name = 'Betty';
+const email = `
+Hi, ${name},
+
+We've been acquired and are shutting down!`
+
+console.log(email === "\n Hi, Betty, \n \n We've been acquired and are shutting down!\n");
+
