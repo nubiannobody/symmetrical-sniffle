7010301f9e1a64a598bd45a90765ed4deb716570
Kween <kweenyoallhiada@gmail.com>
2023-02-07 23:36:47 -0800

    Add users.js

---
+++ users.js
@@ -0,0 +1,13 @@
+const users = [
+    {name: 'Amir', loginCount: 5},
+    {name: 'Betty', loginCount: 16},
+];
+function loginCount(user) {
+    return {[user.name]: user.loginCount};
+}
+[
+    loginCount(users[0]),
+    loginCount(users[1]),
+];
+
+console.log(users);
